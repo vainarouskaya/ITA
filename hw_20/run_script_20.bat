cbc21e15ef82af2152527b20f5b4f011f3628fa0
vainarouskaya <vainarouskaya@gmail.com>
2013-06-10 23:01:11 -0700

    hw_20

---
+++ hw_20/run_script_20.bat
@@ -0,0 +1,4 @@
+:: Command to execute the test
+
+:: ruby ./lib/static_element_validation.rb -d www.epinions.com --exitstatus
+ruby ./lib/epinions_post_qa.rb -f html -o ./reports/epinions_post_qa_test.html
---
+++ hw_20/run_script_20.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Commands to execute script 20.rb
+
+ruby ./lib/epinions_post_qa.rb -f html -o ./reports/epinions_post_qa_test.html
