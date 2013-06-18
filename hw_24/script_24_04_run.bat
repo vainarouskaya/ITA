36724bb88b7e379825fb568b28a85820b0242a2f
vainarouskaya <vainarouskaya@gmail.com>
2013-06-18 00:13:55 -0700

    Homework #24updtd

---
+++ hw_24/script_24_04_run.bat
@@ -0,0 +1,12 @@
+:: Commands to execute Homeworks :: 24
+
+:: ruby lib/script_24_01.rb -i ./etc/input_24.json
+:: ruby lib/script_24_02.rb -i ./etc/input_24.json
+:: ruby lib/script_24_03.rb -i ./etc/input_24.json
+ruby lib/script_24_04.rb -i ./etc/input_24.json
+:: ruby lib/script_24_05.rb -i ./etc/input_24.json
+:: ruby lib/script_24_06a.rb -i ./etc/input_24.json
+:: ruby lib/script_24_06b.rb -i ./etc/input_24.json
+:: ruby lib/script_24_07a.rb -i ./etc/input_24.json
+:: ruby lib/script_24_07b.rb -i ./etc/input_24.json
+:: ruby lib/script_24_08.rb -i ./etc/input_24.json
