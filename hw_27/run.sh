e04cc6602bc6b9ca0983031f510f5231d41223df
vainarouskaya <vainarouskaya@gmail.com>
2013-06-26 23:41:35 -0700

    HW 27

---
+++ hw_27/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This command executes the test of SignUp App v1 and v4
+
+bundle exec ruby ./lib/static_element_validation.rb -d www.khatilov.com -p ./etc/test_specs/khatilovdotcom_page_urls.json
+#bundle exec ruby ./lib/static_element_validation.rb -d www.khatilov.com -p ./etc/test_specs/khatilovdotcom_page_urls_attributes.json
+
+#bundle exec ruby ./lib/static_element_validation.rb -d www.khatilov.com -p ./etc/test_specs/khatilovdotcom_page_urls.json --exitstatus
+#bundle exec ruby ./lib/static_element_validation.rb -d www.khatilov.com -p ./etc/test_specs/khatilovdotcom_page_urls_attributes.json --exitstatus
