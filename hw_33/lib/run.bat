9f09b188ff4995103111f7fb8f68116abd5f5398
vainarouskaya <vainarouskaya@gmail.com>
2013-07-06 22:55:16 -0700

    HW_33updt

--- hw_33/lib/run.bat
+++ hw_33/lib/run.bat
@@ -1,12 +1,12 @@
 :: Commands to execute Homeworks :: 33
 
 
-ruby ./lib/script_33_01.rb -i ./etc/input_33_01.txt -o ./reports/output_33_01.txt
-:: ruby lib/script_21_02.rb -i ./etc/input_21_02.txt
-:: ruby lib/script_21_03.rb -i ./etc/input_21_03.txt
-:: ruby lib/script_21_04.rb -i ./etc/input_21_04.txt
-:: ruby lib/script_21_05.rb -i ./etc/input_21_05.txt
-:: ruby lib/script_21_06a.rb -i ./etc/input_21_06a.txt
-:: ruby lib/script_21_06b.rb -i ./etc/input_21_06b.txt
-:: ruby lib/script_21_07.rb -i ./etc/input_21_07.txt
-:: ruby lib/script_21_08.rb -i ./etc/input_21_08.txt
+::ruby ./lib/script_33_01.rb -i ./etc/input_33_01.txt -o ./reports/output_33_01.txt
+::ruby ./lib/script_33_02.rb -i ./etc/input_33_02.txt -o ./reports/output_33_02.txt
+:: ruby ./lib/script_33_03.rb -i ./etc/input_33_03.txt -o ./reports/output_33_03.txt
+::ruby ./lib/script_33_04.rb -i ./etc/input_33_04.txt -o ./reports/output_33_04.txt
+::ruby ./lib/script_33_05.rb -i ./etc/input_33_05.txt -o ./reports/output_33_05.txt
+::ruby ./lib/script_33_06_a.rb -i ./etc/input_33_06_a.txt -o ./reports/output_33_06_a.txt
+::ruby ./lib/script_33_06_b.rb -i ./etc/input_33_06_b.txt -o ./reports/output_33_06_b.txt
+::ruby ./lib/script_33_07.rb -i ./etc/input_33_07.txt -o ./reports/output_33_07.txt
+ ruby ./lib/script_33_08.rb -i ./etc/input_33_08.txt -o ./reports/output_33_08.txt
